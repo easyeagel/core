f25bba3ba87521bd16ed0ecbc5ec27cd10a8a009
easyeagel <easyeagel@163.com>
2016-01-27 18:14:51 +0800

    加密message包

---
+++ tinyaes.cpp
@@ -0,0 +1,86 @@
+﻿//  Copyright [2014] <lgb (LiuGuangBao)>
+//=====================================================================================
+//
+//      Filename:  tinyaes.cpp
+//
+//   Description:  tinyAES128 简单封装 实现
+//
+//       Version:  1.0
+//       Created:  2014年12月05日 14时15分59秒
+//      Revision:  none
+//      Compiler:  gcc
+//
+//        Author:  lgb (LiuGuangBao), easyeagel@gmx.com
+//  Organization:  ezbty.org
+//
+//=====================================================================================
+//
+
+#include<cstring>
+
+#include"tinyaes.hpp"
+#include"tinyAES128/aes.h"
+
+namespace core
+{
+
+void TinyAES128::encryptStep(const uint8_t* key, const uint8_t* input, uint8_t *output)
+{
+    AES128_ECB_encrypt(&contex_, input, key, output);
+}
+
+void TinyAES128::decryptStep(const uint8_t* key, const uint8_t* input, uint8_t *output)
+{
+    AES128_ECB_decrypt(&contex_, input, key, output);
+}
+
+void TinyAES128::encrypt(const uint8_t* key, const uint8_t* input, size_t sz, uint8_t* output)
+{
+    IntCode::encode(reinterpret_cast<char*>(output), static_cast<uint32_t>(sz));
+    output += 4;
+
+    for(;;sz -= 16, input += 16, output += 16)
+    {
+        if(sz>=16)
+        {
+            encryptStep(key, input, output);
+            continue;
+        }
+
+        if(sz==0)
+            return;
+
+        uint8_t block[16]={};
+        std::memcpy(block, input, sz);
+        encryptStep(key, block, output);
+        return;
+    }
+}
+
+void TinyAES128::decrypt(const uint8_t* key, const uint8_t* input, uint8_t* output)
+{
+    uint32_t destSize=IntCode::decode<uint32_t>(reinterpret_cast<const char*>(input));
+    const size_t sz=encryptSizeGet(destSize);
+    const auto inputEnd=input+sz;
+    input += 4;
+
+    for(; input<inputEnd; input += 16, output += 16, destSize -= 16)
+    {
+        if(destSize>=16)
+        {
+            decryptStep(key, input, output);
+            continue;
+        }
+
+        if(destSize==0)
+            return;
+
+        uint8_t block[16];
+        decryptStep(key, input, block);
+        std::memcpy(output, block, destSize);
+        return;
+    }
+}
+
+}  // namespace mr
+
---
+++ tinyaes.hpp
@@ -0,0 +1,79 @@
+﻿//  Copyright [2014] <lgb (LiuGuangBao)>
+//=====================================================================================
+//
+//      Filename:  tinyaes.hpp
+//
+//   Description:  tinyAES128 简单封装
+//
+//       Version:  1.0
+//       Created:  2014年12月05日 14时00分04秒
+//      Revision:  none
+//      Compiler:  gcc
+//
+//        Author:  lgb (LiuGuangBao), easyeagel@gmx.com
+//  Organization:  ezbty.org
+//
+//=====================================================================================
+//
+
+#pragma once
+
+#include"int.hpp"
+#include"tinyAES128/aes.h"
+
+namespace core
+{
+
+class TinyAES128
+{
+public:
+    void encryptStep(const uint8_t* key, const uint8_t* input, uint8_t *output);
+    void decryptStep(const uint8_t* key, const uint8_t* input, uint8_t *output);
+
+    static constexpr size_t encryptSizeGet(size_t in)
+    {
+        return 4 + 16*(in/16 + ((in%16)==0 ? 0 : 1) );
+    }
+    void encrypt(const uint8_t* key, const uint8_t* input, size_t sz, uint8_t* output);
+
+    template<typename VK, typename VI, typename VO>
+    void encrypt(const VK* key, const VI* input, size_t sz, VO* output)
+    {
+        return encrypt(reinterpret_cast<const uint8_t*>(key),
+            reinterpret_cast<const uint8_t*>(input), sz,
+            reinterpret_cast<uint8_t*>(output)
+        );
+    }
+
+    template<typename V>
+    static size_t decryptSizeGet(const V* input)
+    {
+        return decryptSizeGet(reinterpret_cast<const uint8_t*>(input));
+    }
+
+    static size_t decryptSizeGet(const uint8_t* input)
+    {
+        return IntCode::decode<uint32_t>(reinterpret_cast<const char*>(input));
+    }
+
+    static size_t decryptBlockSizeGet(const uint8_t* input)
+    {
+        return encryptSizeGet(IntCode::decode<uint32_t>(reinterpret_cast<const char*>(input)));
+    }
+
+    void decrypt(const uint8_t* key, const uint8_t* input, uint8_t* output);
+
+    template<typename VK, typename VI, typename VO>
+    void decrypt(const VK* key, const VI* input, VO* output)
+    {
+        return decrypt(reinterpret_cast<const uint8_t*>(key),
+            reinterpret_cast<const uint8_t*>(input),
+            reinterpret_cast<uint8_t*>(output)
+        );
+    }
+private:
+    AES128Context contex_;
+};
+
+}
+
